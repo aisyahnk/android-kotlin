b9f33e2773430f894545e3cc207d49f095882d45
aisyahnk <aisyahnursyahidah@upi.edu>
2022-08-31 13:34:42 +0700

    Menambahkan View binding

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            hitung()
+        }
+    }
+
+    private fun hitung() {
+        Log.d("MainActivity", "tombol berhasil di klik!")
     }
 }
