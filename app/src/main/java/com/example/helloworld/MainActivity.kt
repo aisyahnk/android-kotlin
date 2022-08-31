2f5a68fceae4ea0e5d1f1e28688b886fe18a755b
aisyahnk <aisyahnursyahidah@upi.edu>
2022-08-31 09:33:20 +0700

    Menambahkan data hewan

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data = ArrayList<hewan>().apply {
+            add(hewan("ayam", R.drawable.ayam))
+            add(hewan("bebek", R.drawable.bebek))
+            add(hewan("domba", R.drawable.domba))
+            add(hewan("kambing", R.drawable.kambing))
+            add(hewan("sapi", R.drawable.sapi))
+        }
     }
 }
---
+++ app/src/main/java/com/example/helloworld/hewan.kt
@@ -0,0 +1,6 @@
+package com.example.helloworld
+
+data class hewan(
+    val nama: String,
+    val gambarResId: Int,
+)
