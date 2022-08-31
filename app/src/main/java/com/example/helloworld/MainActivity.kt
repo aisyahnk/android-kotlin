45c5a42c6c33c88080a23f682d1ed94460a46baf
aisyahnk <aisyahnursyahidah@upi.edu>
2022-08-31 09:47:32 +0700

    Menambahkan hewan pertama

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +21,14 @@ class MainActivity : AppCompatActivity() {
             add(hewan("kambing", R.drawable.kambing))
             add(hewan("sapi", R.drawable.sapi))
         }
+
+        imageView = findViewById(R.id.imageView)
+        textView = findViewById(R.id.textView)
+        showData(data[0])
+    }
+
+    private fun showData(hewan: hewan) {
+        imageView.setImageResource(hewan.gambarResId)
+        textView.text = hewan.nama
     }
 }
