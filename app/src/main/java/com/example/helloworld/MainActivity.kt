1f7d73f48038088a96a94f15942f8c6493315cae
aisyahnk <aisyahnursyahidah@upi.edu>
2022-08-31 10:02:16 +0700

    Menambahkan aksi lanjut

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private lateinit var textView: TextView
+    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,13 @@ class MainActivity : AppCompatActivity() {
         imageView = findViewById(R.id.imageView)
         textView = findViewById(R.id.textView)
         showData(data[0])
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            index++
+            index = (index + 1) % data.size 
+            showData(data[index])
+        }
     }
 
     private fun showData(hewan: hewan) {
