3c6c647de7bf6bc4ef1fdae88271bf6ffc70569a
aisyahnk <aisyahnursyahidah@upi.edu>
2022-09-01 09:51:55 +0700

    Menambahkan data hewan

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,12 +10,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data = ArrayList<hewan>().apply {
-            add(hewan("ayam", R.drawable.ayam))
-            add(hewan("bebek", R.drawable.bebek))
-            add(hewan("domba", R.drawable.domba))
-            add(hewan("kambing", R.drawable.kambing))
-            add(hewan("sapi", R.drawable.sapi))
-        }
+        Log.d("MainActivity", "Jumlah Data: ${getData().size}")
+    }
+    private fun getData(): List<hewan> {
+        return listOf(
+            hewan("angsa","Cygnus olor", R.drawable.angsa),
+            hewan("ayam","Gallus gallus", R.drawable.ayam),
+            hewan("bebek", "Cairina moschata",R.drawable.bebek),
+            hewan("domba","Ovis ammon", R.drawable.domba),
+            hewan("kalkun","Meleagris gallopavo", R.drawable.kalkun),
+            hewan("kambing", "Capricornis sumatrensis",R.drawable.kambing),
+            hewan("kelinci","Oryctolagus cuniculus", R.drawable.kelinci),
+            hewan("kerbau","Bubalus bubalis", R.drawable.kerbau),
+            hewan("kuda","Equus caballus", R.drawable.kuda),
+            hewan("sapi", "Bos taurus",R.drawable.sapi),
+        )
     }
 }
--- app/src/main/java/com/example/helloworld/hewan.kt
+++ app/src/main/java/com/example/helloworld/hewan.kt
@@ -2,5 +2,6 @@ package com.example.helloworld
 
 data class hewan(
     val nama: String,
+    val namaLatin: String,
     val gambarResId: Int,
 )
