ed5d16d3f27c027d7cd5e5f4e8c9295dd5f34814
aisyahnk <aisyahnursyahidah@upi.edu>
2022-09-01 09:55:42 +0700

    Menambahkan binding view

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMain.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", "Jumlah Data: ${getData().size}")
